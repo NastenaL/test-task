55f95c9164dc2049789ab030a197b2f9cd43e61f
NLiakh <analiakh17@gmail.com>
2019-01-09 22:28:56 +0200

    llll

--- testTask1/Models/Profile.cs
+++ testTask1/Models/Profile.cs
@@ -7,9 +7,9 @@ namespace testTask1.Models
 {
     public class Profile
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Name { get; set; }
         public DateTime CreateDate { get; set; }
-        public int Views { get; set; }
+        public string Views { get; set; }
     }
 }
